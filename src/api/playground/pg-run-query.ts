520b2fcb8bdf08ade52f7c7d64869f8350cbd9c4
PrasanKumar <prasankumar93@gmail.com>
2025-05-09 10:21:45 +0530

    pgRunQuery to handle user data expiry checks

--- src/api/playground/pg-run-query.ts
+++ src/api/playground/pg-run-query.ts
@@ -9,6 +9,7 @@ import {
   replaceKeyPrefixInQuery,
   replaceKeyPrefixInResult,
 } from "./new-user-data/user-data-config.js";
+import { pgResetUserDataExpiry } from "./new-user-data/pg-reset-user-data-expiry.js";
 
 const pgRunQuery = async (
   input: z.infer<typeof InputSchemas.pgRunQuerySchema>
@@ -21,26 +22,38 @@ const pgRunQuery = async (
       message: `Custom query size exceeds maximum limit ${MAX_CUSTOM_QUERY_SIZE} characters`,
     };
   }
+  const checkUserDataExpiry = input.userId ? true : false;
+  let genUserId = input.userId || "";
+
+  //-----
+  // if no userId, and write command passed , then getNewUserId()
+  //------
 
   const redisWrapperST = RedisWrapperST.getInstance();
   let result: any = {};
 
   if (input.customQuery) {
-    const runQuery = replaceKeyPrefixInQuery(input.customQuery, input.userId);
+    const runQuery = replaceKeyPrefixInQuery(input.customQuery, genUserId);
     result = await redisWrapperST.rawCommandExecute(runQuery);
   } else if (input.queryId) {
     const queryData = await getQueryDataById(input.queryId as QueryIdType);
     if (queryData?.query) {
-      const runQuery = replaceKeyPrefixInQuery(queryData.query, input.userId);
+      const runQuery = replaceKeyPrefixInQuery(queryData.query, genUserId);
       result = await redisWrapperST.rawCommandExecute(runQuery);
     }
   } else {
     throw new Error("No query provided!");
   }
 
-  //TODO: check if userId is passed in input, then call async pgResetUserDataExpiry()
+  result = replaceKeyPrefixInResult(result, genUserId);
 
-  result = replaceKeyPrefixInResult(result, input.userId);
+  if (checkUserDataExpiry) {
+    //(async) check if expiry to be extended when a user query is run
+    pgResetUserDataExpiry({
+      userId: input.userId || "", //userId from input only
+    });
+  }
+
   return result;
 };
 
