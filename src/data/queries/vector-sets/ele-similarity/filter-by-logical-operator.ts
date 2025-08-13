a8d66c6cdc0142bd58f7533f1414e04d692ed344
PrasanKumar <prasankumar93@gmail.com>
2025-08-13 18:37:26 +0530

    added 500 vector set elements rather 2500 plus

--- src/data/queries/vector-sets/ele-similarity/filter-by-logical-operator.ts
+++ src/data/queries/vector-sets/ele-similarity/filter-by-logical-operator.ts
@@ -8,7 +8,7 @@ const queryViewData: IQueryViewData = {
   query: `// Retrieve elements similar to an existing element 's4' (sentence 4) and filter the results by word count
 // Use dot notation to access attribute fields, 
 // Logical: and, or, not (&&, ||, !)
-VSIM '{keyPrefix}' ELE 's4' WITHSCORES WITHATTRIBS FILTER '.wordCount >= 5 and .wordCount < 8'`,
+VSIM '{keyPrefix}' ELE 's4' WITHSCORES WITHATTRIBS FILTER '.wordCount >= 5 and .wordCount < 10'`,
   queryFile: "",
   dbIndexId: "",
   dataSourceId: DATA_SOURCE_ID.STS_DEV_DS,
